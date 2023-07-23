d524f92f4d1534954de2bb3a883c1cdd8a2b60d6
Manav <manav@mrmr.io>
2023-07-23 11:19:05 +0530

    CLI arg

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
+import Data.Maybe
+import System.Environment
+import Text.Read
 import Time
 
 main :: IO ()
-main = foreverNow
+main = do
+  d <- parseDuration
+  printMessage d
+  foreverNow d
+
+parseDuration :: IO Int
+parseDuration = do
+  args <- getArgs
+  return $ fromMaybe 3 $ (listToMaybe args) >>= readMaybeInt
+
+readMaybeInt :: String -> Maybe Int
+readMaybeInt = readMaybe
+
+printMessage :: Int -> IO ()
+printMessage d = putStrLn ("Every " ++ (show d) ++ " seconds I'll remind you, that I exist. I am time.")
--- src/Time.hs
+++ src/Time.hs
@@ -10,8 +10,8 @@ now = do
   t <- Data.Time.getCurrentTime
   return (show t)
 
-foreverNow :: IO ()
-foreverNow = do
+foreverNow :: Int -> IO ()
+foreverNow t = do
   Time.now >>= putStrLn
-  threadDelay (3 * 1_000_000)
-  foreverNow
+  threadDelay (t * 1_000_000)
+  foreverNow t
