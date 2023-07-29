febed2c864a255c9474a364062c963a40b9b1454
Manav <manav@mrmr.io>
2023-07-29 08:39:12 +0530

    Simplify - II

--- app/Main.hs
+++ app/Main.hs
@@ -12,18 +12,7 @@ main = do
   foreverNow d
 
 parseDuration :: [String] -> Int
-parseDuration args = fromMaybe 3 b
-  where
-    a = (listToMaybe :: [String] -> Maybe String) args
-    b = a >>= (readMaybeInt :: String -> Maybe Int)
-
--- return 3
--- return (fromMaybe 3 b)
-
--- eparseDuration = getArgs >>= (\args -> fromMaybe 3 $ readMaybeInt $ listToMaybe args)
-
-readMaybeInt :: String -> Maybe Int
-readMaybeInt = readMaybe
+parseDuration args = fromMaybe 3 (listToMaybe args >>= readMaybe)
 
 printMessage :: Int -> IO ()
 printMessage d = putStrLn ("Every " ++ show d ++ " seconds I'll remind you, that I exist. I am time.")
